707de1e4646caca5c8ac8f538a7e567b4e0ccfd6
Denis Pankov <idenispankov@gmail.com>
2021-03-23 14:14:25 -0700

    register component

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ export default function App() {
           <Header />
 
           <Switch>
-            <Route path='/around'>
+            <Route exact path='/around'>
               <AroundTheUs />
             </Route>
 
