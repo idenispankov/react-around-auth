6f37b306169318e33331391732e83154f5038b24
Denis Pankov <idenispankov@gmail.com>
2021-03-23 16:51:21 -0700

    Complete Reset Project

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className='footer'>
-      <p className='footer__text'>© 2020 Around the U.S.</p>
-    </footer>
-  );
-}
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import logo from '../images/header__logo.svg';
-
-export default function Header() {
-  return (
-    <header className='header'>
-      <img className='header__logo' src={logo} alt='Around US logo' />
-    </header>
-  );
-}
