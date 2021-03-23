a8fdcf759f68f4483a2ca4654ee3c37468426e6f
Denis Pankov <idenispankov@gmail.com>
2021-03-22 20:03:47 -0700

    Protected Route Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,40 @@
-// import { useState, useEffect } from 'react';
-// import { Route, Switch } from 'react-router-dom';
-// import Login from './Login';
+import { useState, useEffect } from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute';
+import Login from './Login';
 import Register from './Register';
 import Header from './Header';
-// import AroundTheUs from './AroundTheUs';
-// import Login from './Login';
+import Footer from './Footer';
+import AroundTheUs from './AroundTheUs';
 import { AccountContext } from '../context/AccountContext';
 
 export default function App() {
+  const history = useHistory();
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [accountData, setAccountData] = useState({ _id: '', email: '' });
+
   return (
     <AccountContext.Provider value={{}}>
       <div className='page'>
         <div className='page__container'>
           <Header />
-          {/* <AroundTheUs /> */}
-          {/* <Login /> */}
-          <Register />
-          {/* <Switch>
+          <Switch>
             <ProtectedRoute exact path='/' component={AroundTheUs} />
+
+            <Route path='/signin'>
+              <Login />
+            </Route>
+
+            <Route path='/signup'>
+              <Register />
+            </Route>
+
+            <Route>
+              {/* {loggedIn ? <Redirect to='/' /> : <Redirect to='/signin' />} */}
+            </Route>
           </Switch>
-          <Route path='/signin'>
-            <Login />
-          </Route> */}
+          <Footer />
         </div>
       </div>
     </AccountContext.Provider>
